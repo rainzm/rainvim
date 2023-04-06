01f1822ad16a8e62ea421a2857fa1e89bce8570c
rainzm <mjoycarry@gmail.com>
2023-04-06 11:13:49 +0800

    add copilot.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -90,4 +90,21 @@ return {
 		"echasnovski/mini.bufremove",
 		lazy = true,
 	},
+	{
+		"github/copilot.vim",
+		init = function()
+			vim.g.copilot_proxy = "127.0.0.1:7890"
+			vim.g.copilot_no_tab_map = true
+			-- imap <silent> <leader>j <Plug>(copilot-next)
+			-- imap <silent> <leader>k <Plug>(copilot-previous)
+			vim.cmd([[
+			    imap <silent><script><expr> <C-n> copilot#Accept("\<CR>")
+                imap <silent> <C-\> <Plug>(copilot-dismiss)
+            ]])
+		end,
+		config = function()
+			-- imap <silent> <C-]> <Plug>(copilot-next)
+			-- imap <silent> <C-[> <Plug>(copilot-previous)
+		end,
+	},
 }
