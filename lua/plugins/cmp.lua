04af34df1f937c2d243eb370e463f0fdfb815f9c
rainzm <mjoycarry@gmail.com>
2023-08-02 14:33:15 +0800

    feat: cancel snip when enter normal mode

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -56,6 +56,19 @@ function M.config()
 	local luasnip = require("luasnip")
 	local cmp = require("cmp")
 	local compare = require("cmp.config.compare")
+
+	-- https://github.com/L3MON4D3/LuaSnip/issues/656
+	local unlinkgrp = vim.api.nvim_create_augroup("UnlinkSnippetOnModeChange", { clear = true })
+	vim.api.nvim_create_autocmd("ModeChanged", {
+		group = unlinkgrp,
+		pattern = { "s:n", "i:*" },
+		desc = "Forget the current snippet when leaving the insert mode",
+		callback = function(evt)
+			if luasnip.session and luasnip.session.current_nodes[evt.buf] and not luasnip.session.jump_active then
+				luasnip.unlink_current()
+			end
+		end,
+	})
 	cmp.setup({
 		snippet = {
 			expand = function(args)
