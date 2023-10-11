37d947c110d6b2290f2b06a723e43e276e3b211b
rainzm <mjoycarry@gmail.com>
2023-10-11 18:02:50 +0800

    feat: format

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,117 +2,117 @@ vim.g.mapleader = "'"
 local M = {}
 
 function M.noremap(mode, shorcut, command)
-	vim.api.nvim_set_keymap(mode, shorcut, command, { noremap = true, silent = true })
+    vim.api.nvim_set_keymap(mode, shorcut, command, { noremap = true, silent = true })
 end
 
 function M.nnoremap(shortcut, command)
-	M.noremap("n", shortcut, command)
+    M.noremap("n", shortcut, command)
 end
 
 function M.vnoremap(shortcut, command)
-	M.noremap("v", shortcut, command)
+    M.noremap("v", shortcut, command)
 end
 
 function M.tnoremap(shorcut, command)
-	M.noremap("t", shorcut, command)
+    M.noremap("t", shorcut, command)
 end
 
 function M.inoremap(shorcut, command)
-	M.noremap("i", shorcut, command)
+    M.noremap("i", shorcut, command)
 end
 
 function M.cnoremap(shorcut, command)
-	M.noremap("c", shorcut, command)
+    M.noremap("c", shorcut, command)
 end
 
 local rhs_options = {}
 
 function rhs_options:new()
-	local instance = {
-		cmd = "",
-		options = { noremap = false, silent = false, expr = false, nowait = false },
-	}
-	setmetatable(instance, self)
-	self.__index = self
-	return instance
+    local instance = {
+        cmd = "",
+        options = { noremap = false, silent = false, expr = false, nowait = false },
+    }
+    setmetatable(instance, self)
+    self.__index = self
+    return instance
 end
 
 function rhs_options:map_cmd(cmd_string)
-	self.cmd = cmd_string
-	return self
+    self.cmd = cmd_string
+    return self
 end
 
 function rhs_options:map_cr(cmd_string)
-	self.cmd = (":%s<CR>"):format(cmd_string)
-	return self
+    self.cmd = (":%s<CR>"):format(cmd_string)
+    return self
 end
 
 function rhs_options:map_args(cmd_string)
-	self.cmd = (":%s<Space>"):format(cmd_string)
-	return self
+    self.cmd = (":%s<Space>"):format(cmd_string)
+    return self
 end
 
 function rhs_options:map_cu(cmd_string)
-	self.cmd = (":<C-u>%s<CR>"):format(cmd_string)
-	return self
+    self.cmd = (":<C-u>%s<CR>"):format(cmd_string)
+    return self
 end
 
 function rhs_options:with_silent()
-	self.options.silent = true
-	return self
+    self.options.silent = true
+    return self
 end
 
 function rhs_options:with_noremap()
-	self.options.noremap = true
-	return self
+    self.options.noremap = true
+    return self
 end
 
 function rhs_options:with_expr()
-	self.options.expr = true
-	return self
+    self.options.expr = true
+    return self
 end
 
 function rhs_options:with_nowait()
-	self.options.nowait = true
-	return self
+    self.options.nowait = true
+    return self
 end
 
 function M.map_cr(cmd_string)
-	local ro = rhs_options:new()
-	return ro:map_cr(cmd_string)
+    local ro = rhs_options:new()
+    return ro:map_cr(cmd_string)
 end
 
 function M.map_cmd(cmd_string)
-	local ro = rhs_options:new()
-	return ro:map_cmd(cmd_string)
+    local ro = rhs_options:new()
+    return ro:map_cmd(cmd_string)
 end
 
 function M.map_cu(cmd_string)
-	local ro = rhs_options:new()
-	return ro:map_cu(cmd_string)
+    local ro = rhs_options:new()
+    return ro:map_cu(cmd_string)
 end
 
 function M.map_args(cmd_string)
-	local ro = rhs_options:new()
-	return ro:map_args(cmd_string)
+    local ro = rhs_options:new()
+    return ro:map_args(cmd_string)
 end
 
 function M.nvim_load_mapping(mapping)
-	for key, value in pairs(mapping) do
-		local mode, keymap = key:match("([^|]*)|?(.*)")
-		if type(value) == "string" then
-			value = M.map_cr(value):with_noremap():with_silent()
-		end
-		if type(value) == "table" and value.f then
-			local m = value.m or "n"
-			vim.keymap.set(m, key, value.f)
-		end
-		if type(value) == "table" and value.cmd then
-			local rhs = value.cmd
-			local options = value.options
-			vim.api.nvim_set_keymap(mode, keymap, rhs, options)
-		end
-	end
+    for key, value in pairs(mapping) do
+        local mode, keymap = key:match("([^|]*)|?(.*)")
+        if type(value) == "string" then
+            value = M.map_cr(value):with_noremap():with_silent()
+        end
+        if type(value) == "table" and value.f then
+            local m = value.m or "n"
+            vim.keymap.set(m, key, value.f)
+        end
+        if type(value) == "table" and value.cmd then
+            local rhs = value.cmd
+            local options = value.options
+            vim.api.nvim_set_keymap(mode, keymap, rhs, options)
+        end
+    end
 end
 
 M.nnoremap("<C-A>", "^")
@@ -130,14 +130,14 @@ M.nnoremap("<C-P>", "<C-O>")
 M.nnoremap("n", ":set hlsearch<cr>n")
 M.nnoremap("N", ":set hlsearch<cr>N")
 M.nnoremap("/", ":set hlsearch<cr>/")
-M.nnoremap("?", ":set hlsearch<cr>?")
+--M.nnoremap("?", ":set hlsearch<cr>?")
 M.nnoremap("*", "*:set hlsearch<cr>")
 M.nnoremap("gm", ":set nohlsearch<cr>")
 
 M.nvim_load_mapping({
-	["n|gwg"] = "ChooseWin",
-	["n|gwc"] = "ChooseWinCopy",
-	["n|gws"] = "ChooseWinSwap",
+    ["n|gwg"] = "ChooseWin",
+    ["n|gwc"] = "ChooseWinCopy",
+    ["n|gws"] = "ChooseWinSwap",
 })
 
 return M
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,130 +1,131 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		version = false, -- last release is way too old and doesn't work on Windows
-		build = ":TSUpdate",
-		-- event = { "BufReadPost", "BufNewFile" },
-		dependencies = {
-			{
-				"nvim-treesitter/nvim-treesitter-textobjects",
-				-- init = function()
-				--   -- PERF: no need to load the plugin, if we only need its queries for mini.ai
-				--   local plugin = require("lazy.core.config").spec.plugins["nvim-treesitter"]
-				--   local opts = require("lazy.core.plugin").values(plugin, "opts", false)
-				--   local enabled = false
-				--   if opts.textobjects then
-				--     for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
-				--       if opts.textobjects[mod].enable then
-				--         enabled = true
-				--         break
-				--       end
-				--     end
-				--   end
-				--   if not enabled then
-				--     require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
-				--   end
-				-- end,
-			},
-			{ "nvim-treesitter/playground" },
-		},
-		-- keys = {
-		--   { "<c-space>", desc = "Increment selection" },
-		--   { "<bs>", desc = "Schrink selection", mode = "x" },
-		-- },
-		---@type TSConfig
-		opts = {
-			highlight = { enable = true, dditional_vim_regex_highlighting = false },
-			--indent = { enable = true, disable = { "python" } },
-			--context_commentstring = { enable = true, enable_autocmd = false },
-			context_commentstring = {
-				enable = true,
-			},
-			ensure_installed = {
-				"bash",
-				"help",
-				"go",
-				"objc",
-				"json",
-				"lua",
-				"markdown",
-				"markdown_inline",
-				"python",
-				"regex",
-				"tsx",
-				"vim",
-				"yaml",
-				"query",
-				"regex",
-			},
-			textobjects = {
-				select = {
-					enable = true,
+    {
+        "nvim-treesitter/nvim-treesitter",
+        version = false, -- last release is way too old and doesn't work on Windows
+        build = ":TSUpdate",
+        -- event = { "BufReadPost", "BufNewFile" },
+        dependencies = {
+            {
+                "nvim-treesitter/nvim-treesitter-textobjects",
+                -- init = function()
+                --   -- PERF: no need to load the plugin, if we only need its queries for mini.ai
+                --   local plugin = require("lazy.core.config").spec.plugins["nvim-treesitter"]
+                --   local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+                --   local enabled = false
+                --   if opts.textobjects then
+                --     for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
+                --       if opts.textobjects[mod].enable then
+                --         enabled = true
+                --         break
+                --       end
+                --     end
+                --   end
+                --   if not enabled then
+                --     require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
+                --   end
+                -- end,
+            },
+            -- { "nvim-treesitter/playground" },
+        },
+        -- keys = {
+        --   { "<c-space>", desc = "Increment selection" },
+        --   { "<bs>", desc = "Schrink selection", mode = "x" },
+        -- },
+        ---@type TSConfig
+        opts = {
+            highlight = { enable = true, dditional_vim_regex_highlighting = false },
+            --indent = { enable = true, disable = { "python" } },
+            --context_commentstring = { enable = true, enable_autocmd = false },
+            context_commentstring = {
+                enable = true,
+            },
+            ensure_installed = {
+                "bash",
+                "help",
+                "go",
+                "objc",
+                "json",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "regex",
+                "tsx",
+                "vim",
+                "yaml",
+                "query",
+                "regex",
+                "rust",
+            },
+            textobjects = {
+                select = {
+                    enable = true,
 
-					-- Automatically jump forward to textobj, similar to targets.vim
-					lookahead = true,
+                    -- Automatically jump forward to textobj, similar to targets.vim
+                    lookahead = true,
 
-					keymaps = {
-						-- You can use the capture groups defined in textobjects.scm
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-						["ac"] = "@class.outer",
-						-- You can optionally set descriptions to the mappings (used in the desc parameter of
-						-- nvim_buf_set_keymap) which plugins like which-key display
-						["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-						-- You can also use captures from other query groups like `locals.scm`
-						["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-					},
-					-- You can choose the select mode (default is charwise 'v')
-					--
-					-- Can also be a function which gets passed a table with the keys
-					-- * query_string: eg '@function.inner'
-					-- * method: eg 'v' or 'o'
-					-- and should return the mode ('v', 'V', or '<c-v>') or a table
-					-- mapping query_strings to modes.
-					selection_modes = {
-						["@parameter.outer"] = "v", -- charwise
-						["@function.outer"] = "V", -- linewise
-						["@class.outer"] = "<c-v>", -- blockwise
-					},
-					-- If you set this to `true` (default is `false`) then any textobject is
-					-- extended to include preceding or succeeding whitespace. Succeeding
-					-- whitespace has priority in order to act similarly to eg the built-in
-					-- `ap`.
-					--
-					-- Can also be a function which gets passed a table with the keys
-					-- * query_string: eg '@function.inner'
-					-- * selection_mode: eg 'v'
-					-- and should return true of false
-					include_surrounding_whitespace = true,
-				},
-				move = {
-					enable = true,
-					set_jumps = true, -- whether to set jumps in the jumplist
-					goto_next_end = {
-						["g]f"] = "@function.outer",
-						["g]c"] = "@class.outer",
-					},
-					goto_previous_start = {
-						["g[f"] = "@function.outer",
-						["g[c"] = "@class.outer",
-					},
-				},
-			},
-			-- incremental_selection = {
-			--   enable = true,
-			--   keymaps = {
-			--     init_selection = "<C-space>",
-			--     node_incremental = "<C-space>",
-			--     scope_incremental = "<nop>",
-			--     node_decremental = "<bs>",
-			--   },
-			-- },
-		},
-		---@param opts TSConfig
-		config = function(_, opts)
-			-- local ft_to_parser = require("nvim-treesitter.parsers").filetype_to_parsername
-			-- ft_to_parser.vimwiki = "markdown"
-			require("nvim-treesitter.configs").setup(opts)
-		end,
-	},
+                    keymaps = {
+                        -- You can use the capture groups defined in textobjects.scm
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        -- You can optionally set descriptions to the mappings (used in the desc parameter of
+                        -- nvim_buf_set_keymap) which plugins like which-key display
+                        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                        -- You can also use captures from other query groups like `locals.scm`
+                        ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+                    },
+                    -- You can choose the select mode (default is charwise 'v')
+                    --
+                    -- Can also be a function which gets passed a table with the keys
+                    -- * query_string: eg '@function.inner'
+                    -- * method: eg 'v' or 'o'
+                    -- and should return the mode ('v', 'V', or '<c-v>') or a table
+                    -- mapping query_strings to modes.
+                    selection_modes = {
+                        ["@parameter.outer"] = "v", -- charwise
+                        ["@function.outer"] = "V", -- linewise
+                        ["@class.outer"] = "<c-v>", -- blockwise
+                    },
+                    -- If you set this to `true` (default is `false`) then any textobject is
+                    -- extended to include preceding or succeeding whitespace. Succeeding
+                    -- whitespace has priority in order to act similarly to eg the built-in
+                    -- `ap`.
+                    --
+                    -- Can also be a function which gets passed a table with the keys
+                    -- * query_string: eg '@function.inner'
+                    -- * selection_mode: eg 'v'
+                    -- and should return true of false
+                    include_surrounding_whitespace = true,
+                },
+                move = {
+                    enable = true,
+                    set_jumps = true, -- whether to set jumps in the jumplist
+                    goto_next_end = {
+                        ["]f"] = "@function.outer",
+                        ["]c"] = "@class.outer",
+                    },
+                    goto_previous_start = {
+                        ["[f"] = "@function.outer",
+                        ["[c"] = "@class.outer",
+                    },
+                },
+            },
+            -- incremental_selection = {
+            --   enable = true,
+            --   keymaps = {
+            --     init_selection = "<C-space>",
+            --     node_incremental = "<C-space>",
+            --     scope_incremental = "<nop>",
+            --     node_decremental = "<bs>",
+            --   },
+            -- },
+        },
+        ---@param opts TSConfig
+        config = function(_, opts)
+            -- local ft_to_parser = require("nvim-treesitter.parsers").filetype_to_parsername
+            -- ft_to_parser.vimwiki = "markdown"
+            require("nvim-treesitter.configs").setup(opts)
+        end,
+    },
 }
