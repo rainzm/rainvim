a03210ae4dcf618597e756ef808897c56c9f31e5
rainzm <mjoycarry@gmail.com>
2024-03-27 23:06:10 +0800

    feat: better cmp
    将 cmp 的不同文件的配置合并，自己实现了一个 compare_rime:
    
    rime 优先，rime 项里面按照大小排序

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -19,6 +19,29 @@ function M.init()
 	vim.o.completeopt = "menu,menuone"
 end
 
+---@type cmp.ComparatorFunction
+function M.compare_rime(entry1, entry2)
+	local entry1_rime = entry1 and entry1.source.name == "nvim_lsp" and entry1.source.source.client.name == "rime_ls"
+	local entry2_rime = entry2 and entry2.source.name == "nvim_lsp" and entry2.source.source.client.name == "rime_ls"
+	if entry1_rime and entry2_rime then
+		if entry1.completion_item.sortText and entry2.completion_item.sortText then
+			local diff = vim.stricmp(entry1.completion_item.sortText, entry2.completion_item.sortText)
+			if diff < 0 then
+				return true
+			elseif diff > 0 then
+				return false
+			end
+		end
+	end
+	if entry1_rime then
+		return true
+	end
+	if entry2_rime then
+		return false
+	end
+	return nil
+end
+
 function M.config()
 	local kind_icons = require("plugins.utils.icons").kinds
 
@@ -50,15 +73,61 @@ function M.config()
 				require("luasnip").lsp_expand(args.body)
 			end,
 		},
+		sorting = {
+			comparators = {
+				M.compare_rime,
+				compare.offset,
+				compare.exact,
+				compare.score,
+				compare.recently_used,
+				compare.locality,
+				compare.kind,
+				compare.length,
+				compare.order,
+			},
+		},
 		mapping = {
 			["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
 			["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
 			["<C-Space>"] = cmp.mapping.complete(),
-			["<C-e>"] = cmp.mapping.close(),
-			["<CR>"] = cmp.mapping.confirm({
-				behavior = cmp.ConfirmBehavior.Replace,
-				select = true,
-			}),
+			-- ["<C-e>"] = cmp.mapping.close(),
+			-- ["<CR>"] = cmp.mapping.confirm({
+			-- 	behavior = cmp.ConfirmBehavior.Replace,
+			-- 	select = true,
+			-- }),
+			["<C-e>"] = cmp.mapping.abort(),
+			["<CR>"] = cmp.mapping(function(fallback)
+				local entry = cmp.get_selected_entry()
+				if entry == nil then
+					entry = cmp.core.view:get_first_entry()
+				end
+				if entry and entry.source.name == "nvim_lsp" and entry.source.source.client.name == "rime_ls" then
+					cmp.abort()
+				else
+					if entry ~= nil then
+						cmp.confirm({
+							behavior = cmp.ConfirmBehavior.Replace,
+							select = true,
+						})
+					else
+						fallback()
+					end
+				end
+			end, { "i", "s" }),
+			["<Space>"] = cmp.mapping(function(fallback)
+				local entry = cmp.get_selected_entry()
+				if entry == nil then
+					entry = cmp.core.view:get_first_entry()
+				end
+				if entry and entry.source.name == "nvim_lsp" and entry.source.source.client.name == "rime_ls" then
+					cmp.confirm({
+						behavior = cmp.ConfirmBehavior.Replace,
+						select = true,
+					})
+				else
+					fallback()
+				end
+			end, { "i", "s" }),
 			["<C-d>"] = function()
 				if cmp.visible_docs() then
 					cmp.close_docs()
@@ -77,15 +146,6 @@ function M.config()
 					fallback()
 				end
 			end, { "i", "s" }),
-			-- ["<S-Tab>"] = cmp.mapping(function(fallback)
-			-- 	if cmp.visible() then
-			-- 		cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-			-- 	elseif luasnip.jumpable(-1) then
-			-- 		luasnip.jump(-1)
-			-- 	else
-			-- 		fallback()
-			-- 	end
-			-- end, { "i", "s" }),
 		},
 		formatting = {
 			format = function(entry, vim_item)
@@ -99,7 +159,14 @@ function M.config()
 					luasnip = "[LuaSnip]",
 					nvim_lua = "[Lua]",
 					latex_symbols = "[LaTeX]",
+					neorg = "[Neorg]",
 				})[entry.source.name]
+				if entry.source.name == "nvim_lsp" and entry.source.source.client.name == "rime_ls" then
+					vim_item.kind = ""
+					vim_item.menu = "[Rime]"
+					-- else
+					-- vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind)
+				end
 				return vim_item
 			end,
 		},
@@ -109,14 +176,12 @@ function M.config()
 			},
 		},
 		window = {
-			-- documentation = cmp.config.disable,
 			documentation = cmp.config.window.bordered(),
 		},
 		preselect = cmp.PreselectMode.None,
 		completion = {
 			completeopt = "menu,menuone",
 		},
-		-- preselect = cmp.PreselectMode.None,
 		sources = {
 			{ name = "nvim_lsp" },
 			{ name = "luasnip" },
@@ -131,156 +196,15 @@ function M.config()
 		},
 	})
 	cmp.setup.filetype({ "TelescopePrompt" }, {
-		sorting = {
-			comparators = {
-				compare.sort_text,
-				compare.offset,
-				compare.exact,
-				compare.score,
-				compare.recently_used,
-				compare.kind,
-				compare.length,
-				compare.order,
-			},
-		},
 		enabled = function()
 			return vim.g.cmp_enabled
 		end,
-		mapping = {
-			["<C-e>"] = cmp.mapping.abort(),
-			["<Space>"] = cmp.mapping(function(fallback)
-				local entry = cmp.get_selected_entry()
-				if entry and entry.source.name == "nvim_lsp" and entry.source.source.client.name == "rime_ls" then
-					cmp.confirm({
-						behavior = cmp.ConfirmBehavior.Replace,
-						select = true,
-					})
-				else
-					fallback()
-				end
-			end, { "i", "s" }),
-			["<CR>"] = cmp.mapping(function(fallback)
-				local entry = cmp.get_selected_entry()
-				if entry == nil then
-					entry = cmp.core.view:get_first_entry()
-				end
-				if entry and entry.source.name == "nvim_lsp" and entry.source.source.client.name == "rime_ls" then
-					cmp.abort()
-				else
-					if entry ~= nil then
-						cmp.confirm({
-							behavior = cmp.ConfirmBehavior.Replace,
-							select = true,
-						})
-					else
-						fallback()
-					end
-				end
-			end, { "i", "s" }),
-		},
-		formatting = {
-			format = function(entry, vim_item)
-				-- Kind icons
-				-- Source
-				-- vim_item.menu = entry:get_completion_item().detail
-				vim_item.menu = ({
-					buffer = "[Buffer]",
-					nvim_lsp = "[LSP]",
-					luasnip = "[LuaSnip]",
-					nvim_lua = "[Lua]",
-					latex_symbols = "[LaTeX]",
-					neorg = "[Neorg]",
-				})[entry.source.name]
-				if entry.source.name == "nvim_lsp" and entry.source.source.client.name == "rime_ls" then
-					-- vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-					vim_item.kind = ""
-					vim_item.menu = "[Rime]"
-					-- else
-					-- vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind)
-				end
-				return vim_item
-			end,
-		},
 		sources = {
-			{ name = "nvim_lsp", priority = 80 },
+			{ name = "nvim_lsp" },
 		},
 	})
-	cmp.setup.filetype({ "markdown", "gitcommit", "norg" }, {
-		sorting = {
-			comparators = {
-				compare.sort_text,
-				compare.offset,
-				compare.exact,
-				compare.score,
-				compare.recently_used,
-				compare.kind,
-				compare.length,
-				compare.order,
-			},
-		},
+	cmp.setup.filetype({ "markdown", "gitcommit", "norg", "copilot-chat" }, {
 		enabled = true,
-		mapping = {
-			["<C-e>"] = cmp.mapping.abort(),
-			-- ["<CR>"] = cmp.mapping.confirm({
-			-- 	behavior = cmp.ConfirmBehavior.Replace,
-			-- 	select = true,
-			-- }),
-			["<Space>"] = cmp.mapping(function(fallback)
-				local entry = cmp.get_selected_entry()
-				if entry == nil then
-					entry = cmp.core.view:get_first_entry()
-				end
-				if entry and entry.source.name == "nvim_lsp" and entry.source.source.client.name == "rime_ls" then
-					cmp.confirm({
-						behavior = cmp.ConfirmBehavior.Replace,
-						select = true,
-					})
-				else
-					fallback()
-				end
-			end, { "i", "s" }),
-			["<CR>"] = cmp.mapping(function(fallback)
-				local entry = cmp.get_selected_entry()
-				if entry == nil then
-					entry = cmp.core.view:get_first_entry()
-				end
-				if entry and entry.source.name == "nvim_lsp" and entry.source.source.client.name == "rime_ls" then
-					cmp.abort()
-				else
-					if entry ~= nil then
-						cmp.confirm({
-							behavior = cmp.ConfirmBehavior.Replace,
-							select = true,
-						})
-					else
-						fallback()
-					end
-				end
-			end, { "i", "s" }),
-		},
-		formatting = {
-			format = function(entry, vim_item)
-				-- Kind icons
-				-- Source
-				-- vim_item.menu = entry:get_completion_item().detail
-				vim_item.menu = ({
-					buffer = "[Buffer]",
-					nvim_lsp = "[LSP]",
-					luasnip = "[LuaSnip]",
-					nvim_lua = "[Lua]",
-					latex_symbols = "[LaTeX]",
-					neorg = "[Neorg]",
-				})[entry.source.name]
-				if entry.source.name == "nvim_lsp" and entry.source.source.client.name == "rime_ls" then
-					-- vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-					vim_item.kind = ""
-					vim_item.menu = "[Rime]"
-					-- else
-					-- vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind)
-				end
-				return vim_item
-			end,
-		},
 		sources = {
 			{ name = "neorg", priority = 90 },
 			{ name = "nvim_lsp", priority = 80 },
