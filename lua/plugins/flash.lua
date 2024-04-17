eeea004da17e99fa7973b1c8e554cb67afbda0eb
rainzm <mjoycarry@gmail.com>
2024-04-18 00:29:46 +0800

    Chinese-English hybrid model using flash-zh.nvim

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,28 +1,5 @@
 return {
 	{
-		"rainzm/flash-zh.nvim",
-		event = "VeryLazy",
-		dependencies = "folke/flash.nvim",
-		keys = {
-			{
-				"s",
-				mode = { "n", "x", "o" },
-				function()
-					require("flash-zh").jump({ search = { wrap = false } })
-				end,
-				desc = "Flash between Chinese",
-			},
-			{
-				"S",
-				mode = { "n", "x", "o" },
-				function()
-					require("flash-zh").jump({ search = { forward = false, wrap = false } })
-				end,
-				desc = "Flash between Chinese",
-			},
-		},
-	},
-	{
 		"folke/flash.nvim",
 		event = "VeryLazy",
 		opts = {
@@ -57,10 +34,18 @@ return {
 		},
         -- stylua: ignore
         keys = {
-            { "f", mode = { "n", "x", "o" }, function() require("flash").jump({ search = { wrap = false } }) end,
-                desc = "Flash" },
-            { "F", mode = { "n", "x", "o" }, function() require("flash").jump({ search = { forward = false, wrap = false } }) end,
-                desc = "Flash" },
+            -- {
+            --     "f",
+            --     mode = { "n", "x", "o" },
+            --     function() require("flash").jump({ search = { wrap = false } }) end,
+            --     desc = "Flash"
+            -- },
+            -- {
+            --     "F",
+            --     mode = { "n", "x", "o" },
+            --     function() require("flash").jump({ search = { forward = false, wrap = false } }) end,
+            --     desc = "Flash"
+            -- },
             {
                 "R",
                 mode = { "n", "x", "o" },
@@ -76,5 +61,39 @@ return {
             --     "Remote Flash"
             -- },
         },
+	},
+	{
+		"rainzm/flash-zh.nvim",
+		event = "VeryLazy",
+		rocks = "luautf8",
+		dependencies = "folke/flash.nvim",
+		keys = {
+			{
+				"f",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash-zh").jump({
+						search = { wrap = false },
+					})
+				end,
+				desc = "Flash between Chinese",
+			},
+			{
+				"F",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash-zh").jump({ search = { forward = false, wrap = false } })
+				end,
+				desc = "Flash between Chinese",
+			},
+			{
+				"s",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash-zh").jump({ chinese_only = false })
+				end,
+				desc = "Flash between Chinese",
+			},
+		},
 	},
 }
